fb38ed9f3306a0c1ababc10f96fbc6aef2730d72
Rakib Linux <rakibulinux@gmail.com>
2022-10-10 21:35:13 +0600

    initial work done

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>I am from Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Statistics.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Statistics = () => {
+  return (
+    <div>
+      <h1>I am from Statistics</h1>
+    </div>
+  );
+};
+
+export default Statistics;
