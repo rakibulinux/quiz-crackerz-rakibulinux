cafb18bd2f0a5706612679214d86e7bbc6e36f3d
Rakib Linux <rakibulinux@gmail.com>
2022-10-11 19:28:15 +0600

    chart added

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useContext } from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  Line,
+  LineChart,
+} from "recharts";
+
+import { TopicsContext } from "./Root";
 
 const Statistics = () => {
+  const topicsData = useContext(TopicsContext);
+  const data = topicsData.data;
+  console.log(data);
   return (
-    <div>
-      <h1>I am from Statistics</h1>
+    <div className="flex justify-center items-center">
+      <LineChart
+        width={380}
+        height={500}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+      </LineChart>
     </div>
   );
 };
