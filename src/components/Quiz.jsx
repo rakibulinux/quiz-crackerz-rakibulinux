f56dc703233081a374abc8c13b37f1152a85cdb4
Rakib Linux <rakibulinux@gmail.com>
2022-10-11 12:32:12 +0600

    display right answer added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -8,10 +8,10 @@ const Quiz = () => {
   console.log(quizData.data.name);
   return (
     <div>
-      <h1 className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 flex items-center justify-center">
+      <h1 className="text-3xl text-cyan-400 px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 flex items-center justify-center">
         Quiz of {quizData.data.name}
       </h1>
-      <div className="px-4 pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-96 lg:pb-20 lg:pt-10 flex items-center justify-center">
+      <div className="px-4 pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-80 lg:pb-20 lg:pt-10 flex items-center justify-center">
         <div className="grid gap-8 row-gap-5 mb-8 grid-cols-1 items-center justify-center">
           {quizzes.map((quiz) => (
             <QuizDetails key={quiz.id} quiz={quiz} />
--- src/components/QuizDetails.jsx
+++ src/components/QuizDetails.jsx
@@ -1,17 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const QuizDetails = ({ quiz }) => {
   const { correctAnswer, id, options, question } = quiz;
-  console.log(correctAnswer, id, options, question);
+  const [answer, setAnswer] = useState([]);
+  let number = 1;
+  const checkQuiz = (option) => {
+    if (option === correctAnswer) {
+      //disable the radio button
+      toast.success("Right Answer", { autoClose: 500 });
+    } else {
+      //enable the radio button
+      toast.error("Wrong Answer", { autoClose: 500 });
+    }
+  };
+  const displayCorrectAnswer = (correctAnswer) => {
+    const quizAnswer = correctAnswer;
+    setAnswer(quizAnswer);
+  };
+  console.log(answer);
   return (
-    <div className="border-sky-300">
-      <h1 className="text-xl font-semibold text-sky-500 py-4">
-        Quiz {1}: {question}
-      </h1>
+    <div className="border shadow-2xl border-cyan-300">
+      <div className="flex items-center justify-between">
+        <h1 className="text-center text-xl font-semibold text-cyan-500 p-4">
+          Quiz {number + 1}: {question}
+        </h1>
+        <div>
+          <button
+            onClick={() => displayCorrectAnswer(correctAnswer)}
+            className="p-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <p className="p-2 text-center text-2xl text-cyan-600">
+        Right Answer for this question:
+        <span className="font-bold text-cyan-900">{answer}</span>
+      </p>
       <div className="flex justify-center flex-col p-3  gap-2">
-        {options.map((option) => (
-          <div className="flex justify-start border border-sky-300 hover:bg-sky-100 gap-y- pl-2">
-            <input type="radio" name="" id="" />
+        {options.map((option, idx) => (
+          <div
+            key={idx}
+            className="flex justify-start border border-cyan-300 hover:bg-cyan-100 gap-y- pl-2"
+          >
+            <input
+              onClick={() => checkQuiz(option)}
+              type="radio"
+              name=""
+              id="options"
+              value={option}
+              className="ml-5 dark:bg-gray-800"
+            />
             <p className="m-2" option={option}>
               {option}
             </p>
